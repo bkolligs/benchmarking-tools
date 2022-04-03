279fff668858f974dae545302690317212773df3
Ben Kolligs <benkolligs@gmail.com>
2022-04-03 12:48:20 -0400

    Feat/timers (#3)
    * Added clang format
    
    * Verified Timer works by inspection, now to automate that
    
    * Removing test artifacts from commits
    
    * Added in basic timing tests to verify the functionality makes proper sense

---
+++ src/bench/scope_timer.h
@@ -0,0 +1,32 @@
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+namespace bench {
+
+/* Specify the level to return time at */
+enum class TimeLevel {
+	seconds = 0,
+	milliseconds,
+	microseconds,
+	nanoseconds
+};
+
+class ScopeTimer {
+    private:
+        /* Since we're using an outstream here we can base references to the
+         * children classes such as ofstream to write to a file, or a
+         * stringstream to maintain a local copy*/
+        std::ostream& out_file_;
+        std::string timer_name_;
+        std::chrono::time_point<std::chrono::high_resolution_clock>
+            start_point_;
+        std::chrono::time_point<std::chrono::high_resolution_clock> end_point_;
+
+    public:
+        ScopeTimer(std::ostream& out, const std::string& name);
+        ~ScopeTimer();
+};
+
+}  // namespace bench
--- tests/test_timer.cpp
+++ tests/test_timer.cpp
@@ -1,4 +1,70 @@
+#include <thread>
 #include "gtest/gtest.h"
+#include "scope_timer.h"
+
+using namespace std::chrono_literals;
+
+class ScopeTimerTest : public ::testing::Test {
+    protected:
+        std::ofstream outfile;
+        std::stringstream outstream;
+
+    public:
+        ScopeTimerTest()
+            : outfile(LOG_DIR "/ScopeTimerTest.txt", std::ofstream::out) {}
+        ~ScopeTimerTest() { outfile.flush(); }
+
+        /*
+         * Get the value of the duration in milliseconds from the timer in the
+         * form of a double instead of a string
+         */
+        double getDuration() {
+            std::string result = outstream.str();
+
+            /* Get the location of the actual duration number in the string */
+            std::size_t time_start = result.find(':') + 1;
+            std::size_t time_end   = result.find("ms");
+            std::string string_duration =
+                result.substr(time_start, time_end - time_start);
+
+            /* Convert it to a double */
+            double duration = std::stod(string_duration);
+            /* Clear the stream */
+            outstream.str("");
+            return duration;
+        }
+};
+
+TEST_F(ScopeTimerTest, TestDuration) {
+    /* Test a print loop takes a certain amount of time */
+    int print_amount = 1E3;
+    {
+        bench::ScopeTimer timer(outstream, "TestPrint");
+        for (int i = 0; i < print_amount; i++) {
+            std::cout << "The current number is: " << i << std::endl;
+        }
+    }
+    double duration_one = getDuration();
+
+    {
+        bench::ScopeTimer timer(outstream, "TestPrintWithoutSTDENDL");
+        for (int i = 0; i < print_amount; i++) {
+            std::cout << "The current number is: " << i << "\n";
+        }
+    }
+    double duration_two = getDuration();
+
+    {
+        bench::ScopeTimer timer(outstream, "TestSleep");
+        std::this_thread::sleep_for(1s);
+    }
+
+    double duration_three = getDuration();
+
+    /* Check the durations make sense */
+    ASSERT_GE(duration_one, duration_two);
+    ASSERT_GE (duration_three,1.0);
+}
 
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
